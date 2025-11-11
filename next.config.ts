90ed69060be583e00b7c1302c92f9e77fe09eaad
cseduardoarx-lab <cseduardoalonso@gmail.com>
2025-11-11 17:39:11 -0300

    config next

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+    images: {
+    formats: ['image/webp', 'image/avif'],
+    qualities: [75, 80, 85, 90], // Adicione as qualidades que você usa
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
 };
 
 export default nextConfig;
